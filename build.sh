640045cdd32b958dd27184856a6c5c6082cb78a4
pouya <pouya@applariat.com>
2017-06-21 15:20:51 -0700

    added conf file

--- build.sh
+++ build.sh
@@ -1,17 +1,25 @@
 #! /bin/sh
 # appLariats generic Apache build script
-# Requirements - A valid httpd.conf file exists within the /src/conf/ dir
-# Copies the httpd.conf file from the /src/conf/ and copies it into /usr/local/apache2/conf/
+
 
 #Log everything in /src/build.log
-logfile=/src/build.log
+logfile=/code/build.log
 exec > $logfile 2>&1
 set -x
 
-#Check for httpd.conf file and throw exception if not present
-if [ -e /src/config/httpd.conf ]
+#check inside code artifact for /code/apache-conf/httpd.conf and use it if exists
+#customizing tomcat through providing apache-conf/httpd.conf as part of the code artifact
+if [ -e /code/apache-conf/httpd.conf ]
 then
-    cp -f /src/config/httpd.conf /usr/local/apache2/conf/httpd.conf
+ cp -f /code/apache-conf/httpd.conf /usr/local/apache2/conf/httpd.conf
+else
+ #look inside /conf for httpd.conf and use it if exists
+ #customizing apache through cloning this github repo and providing conf/httpd.conf
+ if [ -e /conf/httpd.conf ]
+ then
+  cp -f /conf/httpd.conf /usr/local/apache2/conf/httpd.conf
+ fi
+ #if non of the above, default config file inside the image will be used
 fi
 
-cp -rf /src/* /usr/local/apache2/htdocs/
+cp -rf /code/* /usr/local/apache2/htdocs/
